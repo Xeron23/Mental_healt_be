bbe9a38115e698cb83b23838a3a682295e944429
xeron <kaqyword2304@gmail.com>
2025-10-05 12:14:45 +0700

    add domain face detecction

--- src/domains/faceDetection/face-routes.js
+++ src/domains/faceDetection/face-routes.js
@@ -4,6 +4,8 @@ import tryCatch from "../../utils/tryCatcher.js";
 import authToken from "../../middlewares/auth-token-middleware.js";
 import uploadImageUpload from "../../middlewares/upload-image-upload.js";
 import faceController from "./face-controller.js";
+import validateCredentials from "../../middlewares/validate-credentials-middleware.js";
+import { faceSchema, getAllFaceSchema } from "./face-schema.js";
 
 class FaceRoutes extends BaseRoutes {
     routes() {
@@ -12,6 +14,22 @@ class FaceRoutes extends BaseRoutes {
             uploadImageUpload,
             tryCatch(faceController.create)
         ]);
+        this.router.get("/:id", [
+            authToken,
+            validateCredentials(faceSchema, "params"),
+            tryCatch(faceController.show)
+        ]);
+        this.router.delete("/:id", [
+            authToken,
+            validateCredentials(faceSchema, "params"),
+            tryCatch(faceController.delete)
+        ]);
+        this.router.get("/", [
+            authToken,
+            validateCredentials(getAllFaceSchema, "query"),
+            tryCatch(faceController.index)
+        ])
+
     }
 }
 
