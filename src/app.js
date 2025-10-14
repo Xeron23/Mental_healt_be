b69ec96aba11105cadb0c1bb4d0bb423418c3aae
Aria Lent <kaqyword2304@gmail.com>
2025-10-14 18:02:52 +0700

    add routes forum chat to app

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ import meditateRoutes from "./domains/meditation/meditate-routes.js";
 import newsRoutes from "./domains/news/news-routes.js";
 import videoRoutes from "./domains/video/video-routes.js";
 import statsRoutes from "./domains/statistic/stats-routes.js";
+import forumRoutes from "./domains/chat/forum/forum-routes.js";
 
 class ExpressApplication {
     app;
@@ -99,6 +100,7 @@ class ExpressApplication {
         this.app.use("/api/v1/mindful-news", newsRoutes)
         this.app.use("/api/v1/video", videoRoutes)
         this.app.use("/api/v1/statistic", statsRoutes);
+        this.app.use("/api/v1/forum", forumRoutes);
 
     }
 
