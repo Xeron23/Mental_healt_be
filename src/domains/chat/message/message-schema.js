b41dee6510de3e3d0473680c241f716f3273eb97
Aria Lent <kaqyword2304@gmail.com>
2025-10-18 16:04:34 +0700

    add message domains

---
+++ src/domains/chat/message/message-schema.js
@@ -0,0 +1,20 @@
+import Joi from "joi";
+
+const createMessageSchema = Joi.object({
+    content: Joi.string().required().min(5)
+        .messages({
+            "string.base": "content must be text",
+            "string.empty": "content is required",
+            "string.min": "content must be at least 5 words."
+    })
+})
+
+const messageSchema = Joi.object({
+    id: Joi.number().required().min(1)
+        .messages({
+            "number.empty": "id messga is required.",
+            "number.min": "id message must be at least 1 number."
+    })
+})
+
+export  {createMessageSchema, messageSchema};
---
+++ src/domains/chat/message/message-service.js
@@ -0,0 +1,88 @@
+import BaseError from "../../../base_classes/base-error.js";
+import prisma from "../../../config/db.js";
+import { checkTgl } from "../../../utils/checkTanggal.js";
+
+class MessageService{
+    async create(data){
+        if(data.parentId){
+            const checkParent = await prisma.message.findFirst({
+                where: {
+                    id: data.parentId
+                }
+            })
+            if(!checkParent){
+                throw BaseError.notFound("Data parent not found");
+            }
+            if(checkParent.userId === data.userId){
+                throw BaseError.badRequest("Cannot self replay")
+            }
+        }
+
+        const message = await prisma.message.create({
+            data: {
+                userId: data.userId,
+                content: data.content,
+                parentId: data.parentId || null
+            },
+            include: {
+                user: true,
+                replies: true
+            }
+        });
+        if(!message){
+            throw BaseError.badRequest("failed to create message")
+        } 
+
+        return message;
+    }
+
+    async update(data){
+        const checkMessage = await prisma.message.findFirst({
+            where: {id: data.id, userId: data.userId}
+        })
+
+        if(!checkMessage){
+            throw BaseError.notFound("Message not found");
+        }
+
+        if(checkTgl(checkMessage.createdAt)){
+            throw BaseError.badRequest("Tidak bisa update, sudah lewat 5 menit");
+        }
+
+        const messgae = await prisma.message.update({
+            where: {
+                id: data.id,
+                userId: data.userId
+            },
+            data: {
+                userId: data.userId,
+                parentId: data.parentId,
+                content: data.content
+            }
+        })
+        return messgae;
+    }
+
+
+
+    async delete(userId, id){
+        const message = await prisma.message.findFirst({
+            where: {id: id, userId: userId}
+        })
+        if(!message){
+            throw BaseError.notFound("message not found");
+        }
+
+        await prisma.message.delete({
+            where: {
+                id: message.id
+            }
+        });
+
+        return {
+            message: "Message deleted succesfully",
+        }
+    }
+}
+
+export default new MessageService();
