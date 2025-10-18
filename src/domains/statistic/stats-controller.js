dcaa92c2b6858c150dd2b5a8876ab95386b07210
xeron <kaqyword2304@gmail.com>
2025-10-18 23:27:58 +0700

    change implementation for stats face & journal

--- src/domains/statistic/stats-controller.js
+++ src/domains/statistic/stats-controller.js
@@ -13,6 +13,26 @@ class StatsController {
 
         return successResponse(res, stats);
     }
+
+    async getFace(req, res){
+        const userId = req.user.user_id;
+        const {tgl} = req.query
+        const faceStats = await StatsService.getFaceHistory(tgl, userId);
+        if(!faceStats){
+            throw Error("Failed to get face detection stats");
+        }
+        return successResponse(res, faceStats);
+    }
+
+    async getJournaling(req,res){
+        const userId = req.user.user_id;
+        const {tgl} = req.query
+        const journalStats = await StatsService.getJournalHistory(tgl, userId);
+        if(!journalStats){
+            throw Error("Failed to get journaling detection stats");
+        }
+        return successResponse(res, journalStats); 
+    }
 }   
 
 export default new StatsController();
